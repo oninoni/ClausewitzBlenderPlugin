f796c5f93ab4492319d4d5a142dc37fbcd3c3496
Bernhard Sirlinger <bernhard@sirlinger.de>
2017-01-28 02:58:00 +0100

    * Add lookahead option

--- import-export-clausewitz/utils.py
+++ import-export-clausewitz/utils.py
@@ -5,40 +5,61 @@ class BufferReader:
         self.buffer = buffer
         self.__offset__ = 0
 
-    def IsEOF(self):
-        return (self.__offset__ >= len(self.buffer))
+    def IsEOF(self, lookaheadByteCount=0):
+        return ((self.__offset__ + lookaheadByteCount) >= len(self.buffer))
 
-    def NextInt8(self):
-        self.__offset__ += 1
-        return self.buffer[self.__offset__ - 1]
+    def NextInt8(self, lookahead=False):
+        if lookahead:
+            return self.buffer[self.__offset__]
+        else:
+            self.__offset__ += 1
+            return self.buffer[self.__offset__ - 1]
 
-    def NextInt32(self):
-        self.__offset__ += 4
-        return struct.unpack_from("i", self.buffer, self.__offset__ - 4)[0]
+    def NextInt32(self, lookahead=False):
+        if lookahead:
+            return struct.unpack_from("i", self.buffer, self.__offset__)[0]
+        else:
+            self.__offset__ += 4
+            return struct.unpack_from("i", self.buffer, self.__offset__ - 4)[0]
 
-    def NextUInt32(self):
-        self.__offset__ += 4
-        return struct.unpack_from("I", self.buffer, self.__offset__ - 4)[0]
+    def NextUInt32(self, lookahead=False):
+        if lookahead:
+            return struct.unpack_from("I", self.buffer, self.__offset__)[0]
+        else:
+            self.__offset__ += 4
+            return struct.unpack_from("I", self.buffer, self.__offset__ - 4)[0]
 
-    def NextFloat32(self):
-        self.__offset__ += 4
-        return struct.unpack_from("f", self.buffer, self.__offset__ - 4)[0]
+    def NextFloat32(self, lookahead=False):
+        if lookahead:
+            return struct.unpack_from("f", self.buffer, self.__offset__)[0]
+        else:
+            self.__offset__ += 4
+            return struct.unpack_from("f", self.buffer, self.__offset__ - 4)[0]
 
-    def NextChar(self):
-        self.__offset__ += 1
-        return chr(self.buffer[self.__offset__ - 1])
+    def NextChar(self, lookahead=False):
+        if lookahead:
+            return chr(self.buffer[self.__offset__])
+        else:
+            self.__offset__ += 1
+            return chr(self.buffer[self.__offset__ - 1])
 
     def GetCurrentOffset(self):
         return self.__offset__
 
-def ReadNullByteString(buffer: BufferReader):
-    stringValue = ""
-        
+def ReadNullByteString(buffer: BufferReader, lookahead=False):
+    stringValue = ""    
+
     char = buffer.NextChar()
         
+    if lookahead:
+        offset = buffer.GetCurrentOffset()
+
     while char != "\x00":
         stringValue += char
         char = buffer.NextChar()
+
+    if lookahead:
+        buffer.__offset__ = offset
 
     return stringValue
 
